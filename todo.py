4c641a95c9fd0a29f7eaf7d0b4f990ac1aaf2a5b
Andrew <andrewliang.dl@gmail.com>
2017-02-15 08:00:45 +1300

    todo list

---
+++ todo.py
@@ -0,0 +1,13 @@
+#TODO Clean up guimain.py
+
+#TODO IP config dialogue
+
+#TODO Start / Stop as checkbox
+
+#TODO Command button set
+
+#TODO Save units
+
+#TODO Write access check
+
+#TODO BUG: Rapidly start/stop polling crash
